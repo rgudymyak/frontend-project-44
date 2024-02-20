e24d90019bdb9b6cb0ed1bf5042173417cdaec3b
rgudymyak <rgudymyak@gmail.com>
2024-02-20 12:10:23 +0700

    fix brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-import nameQuestion from '../src/games/cli.js';
+import { userName } from '../src/index.js';
 
-const brainGames = () => {
-  console.log('Welcome to the Brain Games!');
-  nameQuestion();
-};
-
-export default brainGames;
+userName();
