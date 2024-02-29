25da439fa65bac585104c77dc91afeaa9fd9a190
rgudymyak <rgudymyak@gmail.com>
2024-02-29 22:01:02 +0700

    fix src/games/prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,27 +2,10 @@ import { startGame } from '../index.js';
 import { getRandomNumber } from '../utils.js';
 
 const isPrime = (n) => {
-  if (n < 1) {
+  if (n <= 1) {
     return false;
   }
-
-  if (n === 2 || n === 3) {
-    return true;
-  }
-
-  if (n % 2 === 0 || n % 3 === 0) {
-    return false;
-  }
-
-  const sqrt = Math.sqrt(n);
-
-  for (let i = 5; i <= sqrt; i += 6) {
-    if (n % i === 0) {
-      return false;
-    }
-  }
-
-  for (let i = 7; i <= sqrt; i += 6) {
+  for (let i = 2; i <= Math.sqrt(n); i += 1) {
     if (n % i === 0) {
       return false;
     }
